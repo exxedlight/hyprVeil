69d03499fb54516e15288a24c418b2f755c7ab47
exxedlight <exxedlight@gmail.com>
2026-02-16 03:45:45 +0200

    some directories added

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias mysql='mariadb --prompt="mysql [\d]> "'
+
+if not set -q SSH_AGENT_PID
+    eval (ssh-agent -c >/dev/null)
+end
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
